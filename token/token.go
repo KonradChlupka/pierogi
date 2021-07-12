4a3b79e2527a4fecab312afdd9e4e62c524ffff0
Konrad Chlupka <chlupkaw@gmail.com>
2021-07-12 20:03:59 +0100

    Add support for keywords, identifiers, numbers, and skipping whitespace

--- token/token.go
+++ token/token.go
@@ -38,3 +38,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]Type{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) Type {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
