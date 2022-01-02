80907884cbe5124c06ff2d4af72d86a2622a2a4b
Konrad Chlupka <chlupkaw@gmail.com>
2022-01-02 14:18:22 +0000

    Add error checking to parser test

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"github.com/KonradChlupka/pierogi/ast"
 	"github.com/KonradChlupka/pierogi/lexer"
 	"github.com/KonradChlupka/pierogi/token"
@@ -9,18 +10,30 @@ import (
 type Parser struct {
 	l *lexer.Lexer
 
+	errors []string
+
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+// peekError records a peek error.
+func (p *Parser) peekError(t token.Type) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
