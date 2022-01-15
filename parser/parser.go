56bba78b3ccc30eedc512ba2a7fc65b4b69fc2c7
Konrad Chlupka <chlupkaw@gmail.com>
2022-01-15 21:11:10 +0000

    Parse integer literals

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"github.com/KonradChlupka/pierogi/ast"
 	"github.com/KonradChlupka/pierogi/lexer"
 	"github.com/KonradChlupka/pierogi/token"
+	"strconv"
 )
 
 type (
@@ -48,12 +49,27 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.Type]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	return p
 }
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+
+	return lit
 }
 
 func (p *Parser) Errors() []string {
