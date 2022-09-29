6734e6cbad8693285eef254b71c493b27cb1eaaa
Konrad Chlupka <chlupkaw@gmail.com>
2022-09-29 09:55:53 +0100

    Add if else parsing

--- ast/ast.go
+++ ast/ast.go
@@ -2,7 +2,6 @@ package ast
 
 import (
 	"bytes"
-
 	"github.com/KonradChlupka/pierogi/token"
 )
 
@@ -104,6 +103,24 @@ func (es *ExpressionStatement) String() string {
 	return ""
 }
 
+// BlockStatement represents a series of statements.
+type BlockStatement struct {
+	Token      token.Token // The { token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode()       {}
+func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token // the token.IDENT token
 	Value string
@@ -170,3 +187,28 @@ type Boolean struct {
 func (b *Boolean) expressionNode()      {}
 func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
 func (b *Boolean) String() string       { return b.Token.Literal }
+
+type IfExpression struct {
+	Token       token.Token // The 'if' token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) expressionNode()      {}
+func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
